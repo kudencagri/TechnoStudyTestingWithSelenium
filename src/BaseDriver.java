0347d99d26f3574b8075be4acc81e00c53e71ff9
mustafayumrutepe <mstfyumrutepe@gmail.com>
2024-07-05 20:28:53 +0300

    basedriver was deleted, I added it again

--- src/BaseDriver.java
+++ src/BaseDriver.java
@@ -4,35 +4,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class BaseDriver {
 
-public static WebDriver driver;
-public static WebDriverWait wait;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
-static {
-    Logger logger=Logger.getLogger("");
-    logger.setLevel(Level.SEVERE);
+    static {
+        Logger logger=Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
 
-    driver=new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
 
-    wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        wait=new WebDriverWait(driver,Duration.ofSeconds(20));
 
-    driver.get("https://techno.study/tr/");
-}
-public static void WaitAndQuit(){
-OptionalWait(5);
-driver.quit();
-}
-
-public static void OptionalWait(int sec){
-    try {
-    Thread.sleep(sec*1000l);
-    } catch (InterruptedException e) {
-        throw new RuntimeException(e);
+        driver.get("https://techno.study/tr/");
     }
-}
+    public static void WaitAndQuit(){
+        OptionalWait(5);
+        driver.quit();
+    }
+
+    public static void OptionalWait(int sec){
+        try {
+            Thread.sleep(sec*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
